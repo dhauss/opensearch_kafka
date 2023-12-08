e7e02a1bba36425702bd848727b7963d9cfa44a2
dhauss <davidkhauss@gmail.com>
2023-12-08 11:56:04 -0500

    OpenSearchConsumer implemented with BonsaiConfig class

--- src/main/java/opensearch/kafka/OpenSearchConsumer.java
+++ src/main/java/opensearch/kafka/OpenSearchConsumer.java
@@ -1,7 +1,58 @@
 package opensearch.kafka;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.opensearch.action.search.SearchRequest;
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.index.query.QueryBuilders;
+import org.opensearch.search.builder.SearchSourceBuilder;
+
+import java.net.URI;
+
 public class OpenSearchConsumer {
     public static void main(String[] args) {
+        BonsaiConfig bc = new BonsaiConfig("src/main/resources/bonsai_config.txt");
+        String connString = bc.getConnString();
 
+        URI connUri = URI.create(connString);
+        String[] auth = connUri.getUserInfo().split(":");
+
+        CredentialsProvider cp = new BasicCredentialsProvider();
+        cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+        RestHighLevelClient rhlc = new RestHighLevelClient(
+                RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                        .setHttpClientConfigCallback(
+                                httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp)
+                                        .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
+
+        // https://www.elastic.co/guide/en/elasticsearch/client/java-rest/master/java-rest-high-search.html
+        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+
+        try {
+            SearchResponse resp  = rhlc.search(searchRequest, RequestOptions.DEFAULT);
+            // Show that the query worked
+            System.out.println(resp.toString());
+        } catch (Exception ex) {
+            // Log the exception
+            System.out.println(ex.toString());
+        }
+
+        // Need to close the client so the thread will exit
+        try {
+            rhlc.close();
+        } catch (Exception ex) {
+
+        }
     }
 }
