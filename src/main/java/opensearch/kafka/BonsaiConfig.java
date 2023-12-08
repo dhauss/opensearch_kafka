3e78e1d009d08aa041714eba04a628b37ba4167f
dhauss <davidkhauss@gmail.com>
2023-12-08 11:59:43 -0500

    comments added, code formatting

--- src/main/java/opensearch/kafka/BonsaiConfig.java
+++ src/main/java/opensearch/kafka/BonsaiConfig.java
@@ -4,7 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
+/*
+ * BonsaiConfig has one constructor which takes a filepath that should reference a Bonsai config file.
+ * Constructor reads in the single line of the file and initializes connString attribute. This can then
+ * be used to create a Bonsai client without exposing sensitive data in plain text
+ * (e.g. on this public Github repo)
+ */
 public class BonsaiConfig {
     private String connString;
 
@@ -14,17 +19,14 @@ public class BonsaiConfig {
             FileReader config_fr = new FileReader(config_file);
             BufferedReader config_br = new BufferedReader((config_fr));
             StringBuffer config_sb = new StringBuffer();
+            // file consists of single line- connection URI
             this.setConnString(config_br.readLine());
         } catch(IOException e){
             e.printStackTrace();
         }
     }
 
-    public String getConnString() {
-        return connString;
-    }
+    public String getConnString() { return connString; }
 
-    public void setConnString(String connString) {
-        this.connString = connString;
-    }
+    public void setConnString(String connString) { this.connString = connString; }
 }
