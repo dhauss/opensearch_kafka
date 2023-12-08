49f601cd011f466cebc1985b6f2e77434ad8b7e9
dhauss <davidkhauss@gmail.com>
2023-12-08 11:55:40 -0500

    BonsaiConfig class implemented

--- src/main/java/opensearch/kafka/BonsaiConfig.java
+++ src/main/java/opensearch/kafka/BonsaiConfig.java
@@ -1,2 +1,30 @@
-package opensearch.kafka;public class BonsaiConfig {
+package opensearch.kafka;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BonsaiConfig {
+    private String connString;
+
+    public BonsaiConfig(String filepath){
+        try {
+            File config_file = new File(filepath);
+            FileReader config_fr = new FileReader(config_file);
+            BufferedReader config_br = new BufferedReader((config_fr));
+            StringBuffer config_sb = new StringBuffer();
+            this.setConnString(config_br.readLine());
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public String getConnString() {
+        return connString;
+    }
+
+    public void setConnString(String connString) {
+        this.connString = connString;
+    }
 }
